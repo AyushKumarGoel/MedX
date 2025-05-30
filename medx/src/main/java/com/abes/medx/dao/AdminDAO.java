24de2e0d9de67deddee24945ee127d8974fe8b0a
Suyashigupta2701 <suyashig2701@gmail.com>
2025-05-30 06:04:00 +0530

    admin implemented

--- medx/src/main/java/com/abes/medx/dao/AdminDAO.java
+++ medx/src/main/java/com/abes/medx/dao/AdminDAO.java
@@ -1,10 +1,12 @@
 package com.abes.medx.dao;
 
+import com.abes.medx.dto.AdminDTO;
+
 public interface AdminDAO {
 
     boolean login(String adminId, String password);
     
-    void addDoctor(String doctorId, String name, String specialization);
+    void addDoctor(String doctorId, String name, String specialization,int yearsOfExperience);
     
     void removeDoctor(String doctorId);
     
