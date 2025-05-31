c1c2b1bbd47cd08765217cf7faab67233743a57e
Tanya Srivastava <tanya.srivastava2310@gmail.com>
2025-05-31 14:13:43 +0530

    dto change

--- medx/src/main/java/com/abes/medx/dto/AdminDTO.java
+++ medx/src/main/java/com/abes/medx/dto/AdminDTO.java
@@ -1,10 +1,4 @@
 package com.abes.medx.dto;
-
-import java.lang.reflect.Field;
-import java.util.Iterator;
-import java.util.List;
-
-// import com.abes.medx.exception.UserException;
 public class AdminDTO extends UserDTO {
 
     private String adminId;
@@ -22,38 +16,6 @@ public class AdminDTO extends UserDTO {
         this.adminId = adminId;
     }
 
-    public void showProfile() {
-        System.out.println("Admin Profile:");
-        System.out.println("Admin ID: " + adminId);
-        System.out.println("Name: " + getName());
-        System.out.println("Email: " + getEmail());
-        System.out.println("Phone: " + getPhoneNumber());
-        System.out.println("Age: " + getAge());
-    }
-
-    public boolean removeDoctor(List<DoctorDTO> doctors, String doctorId) {
-        try {
-            Field doctorIdField = DoctorDTO.class.getDeclaredField("doctorId");
-            doctorIdField.setAccessible(true);
-
-            Iterator<DoctorDTO> iterator = doctors.iterator();
-            while (iterator.hasNext()) {
-                DoctorDTO doctor = iterator.next();
-                Object idValue = doctorIdField.get(doctor);
-                if (idValue != null && idValue.toString().equals(doctorId)) {
-                    iterator.remove();
-                    System.out.println("Doctor with ID " + doctorId + " removed.");
-                    return true;
-                }
-            }
-
-            System.out.println("Doctor with ID " + doctorId + " not found.");
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            System.out.println("Reflection error: " + e.getMessage());
-        }
-
-        return false;
-    }
 
     @Override
     public String toString() {
